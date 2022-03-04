474ca67a0883b1fd55ce742a2237c35e9c5f025a
Movie World <100509655+ATHIF-EFX@users.noreply.github.com>
2022-03-04 18:51:12 +0530

    Update __init__.py

--- Alita/__init__.py
+++ Alita/__init__.py
@@ -41,9 +41,9 @@ if version_info[0] < 3 or version_info[1] < 7:
 # the secret configuration specific things
 try:
     if environ.get("ENV"):
-        from alita.vars import Config
+        from Alita.vars import Config
     else:
-        from alita.vars import Development as Config
+        from Alita.vars import Development as Config
 except Exception as ef:
     LOGGER.error(ef)  # Print Error
     LOGGER.error(format_exc())
